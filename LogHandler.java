d5b14a310b3879d3866ce43c4e23e9b44368486f
DA339A <alexgiang@hotmail.com>
2017-03-15 20:30:47 +0100

    Update LogHandler.java

--- LogHandler.java
+++ LogHandler.java
@@ -5,13 +5,22 @@ import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-import javax.swing.ImageIcon;
+/**
+ * Logs activities in a text file.
+ * @author Alex
+ *
+ */
 
 public class LogHandler {
 
 	private Logger log;
 	private FileHandler fh;
 
+	/**
+	 * Creates a logger
+	 * @throws SecurityException
+	 * @throws IOException
+	 */
 	public LogHandler() throws SecurityException, IOException {
 		log = Logger.getLogger("Serverlog");
 		fh = new FileHandler("serverlog.log", true);
@@ -20,6 +29,12 @@ public class LogHandler {
 		// log.setUseParentHandlers(false);
 
 	}
+	
+	/**
+	 * Logs a message with the sender´s name in a logfile
+	 * @param message the sent message
+	 * @param sender the sender
+	 */
 
 	public void logMessage(Message message, String sender) {
 
@@ -27,13 +42,22 @@ public class LogHandler {
 		log.info(message.getMsg());
 
 	}
+	/**
+	 * Logs user activity
+	 * 
+	 * @param string
+	 *            the string to log
+	 */
 
 	public void logServerMessage(String string) {
 
-		log.warning(string);
+		log.info(string);
 
 	}
-
+	/**
+	 * Method for logging errors.
+	 * @param e the error
+	 */
 	public void logError(Exception e) {
 		log.severe("" + e);
 	}
